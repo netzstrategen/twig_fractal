e981df764586142cfa6a69dc3effbfc04986f5ae
Fabian Marz <fabian@netzstrategen.com>
2017-08-17 11:48:23 +0200

    Added method override from 'Twig_TokenParser_Include' .

--- src/TokenParser/Render.php
+++ src/TokenParser/Render.php
@@ -2,6 +2,49 @@
 
 namespace Drupal\twig_fractal\TokenParser;
 
+use Twig_Token;
+use Twig_TokenParser;
+
 class Render extends Twig_TokenParser {
+
+  public function parse(Twig_Token $token)
+  {
+    $expr = $this->parser->getExpressionParser()->parseExpression();
+
+    list($variables, $only, $ignoreMissing) = $this->parseArguments();
+
+    return new Twig_Node_Include($expr, $variables, $only, $ignoreMissing, $token->getLine(), $this->getTag());
+  }
+
+  protected function parseArguments()
+  {
+    $stream = $this->parser->getStream();
+
+    $ignoreMissing = false;
+    if ($stream->nextIf(Twig_Token::NAME_TYPE, 'ignore')) {
+      $stream->expect(Twig_Token::NAME_TYPE, 'missing');
+
+      $ignoreMissing = true;
+    }
+
+    $variables = null;
+    if ($stream->nextIf(Twig_Token::NAME_TYPE, 'with')) {
+      $variables = $this->parser->getExpressionParser()->parseExpression();
+    }
+
+    $only = false;
+    if ($stream->nextIf(Twig_Token::NAME_TYPE, 'only')) {
+      $only = true;
+    }
+
+    $stream->expect(Twig_Token::BLOCK_END_TYPE);
+
+    return array($variables, $only, $ignoreMissing);
+  }
+
+  public function getTag()
+  {
+    return 'include';
+  }
 
 }
