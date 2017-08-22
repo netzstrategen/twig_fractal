35381ddec5799788bfb16257cc0eb64f1208860b
Fabian Marz <fabian@netzstrategen.com>
2017-08-22 16:21:25 +0200

    Added class/method documentation.

--- src/Node/Render.php
+++ src/Node/Render.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @file
+ * Contains Drupal\twig_fractal\Node\Render.
+ */
 
 namespace Drupal\twig_fractal\Node;
 
@@ -9,101 +13,156 @@ use Twig_Node_Expression;
 use Twig_Node_Expression_Constant;
 use Twig_Node_Include;
 
+/**
+ * Class to implement the compiling of the new `render` node.
+ *
+ * This class gets the default template variables from the Fractal component
+ * definition file and adds them to the compiled Twig PHP template string.
+ *
+ * Default variables consist of
+ *
+ * 1. the default `context` properties in the component's definition file,
+ *    which may be overridden by
+ * 2. the `context` properties of the requested variants (if any) in the
+ *    component's definition file, which may be overridden by
+ * 3. the variables passed to the `render` tag itself.
+ */
 class Render extends Twig_Node_Include {
 
   /**
-   * Holds the given component name from the render tag.
+   * The name of the component to render.
    */
   protected $component;
 
+  /**
+   * Constructs a Twig template to render.
+   */
   public function __construct(Twig_Node_Expression $expr, Twig_Node_Expression $variables = NULL, $only = FALSE, $ignoreMissing = FALSE, $lineno, $tag = NULL) {
     $this->component = $expr->getAttribute('value');
-    // Remove variant appendix from component name as we do not have variant files.
-    // @todo check if variant files are available before sanitize the name
-    $expr->setAttribute('value', $this->sanitizeComponentName($this->component));
-    // Add an 'not_used' node if we don't have any variables to render correctly.
-    if (NULL === $variables) {
-      $variables = new Twig_Node_Expression_Constant(['not_used'], $lineno);
-    }
+    // Remove any variant suffixes from the template name as there are no
+    // template files for variants, only for components.
+    $expr->setAttribute('value', $this->extractComponentName($this->component));
     parent::__construct($expr, $variables, $only, $ignoreMissing, $lineno);
   }
 
   /**
-   * Adds the template arguments including the component (variant) context.
+   * Adds the template variables to the compiled Twig PHP template string.
+   *
+   * Variables consist of
+   *
+   * 1. the default `context` properties in the component's definition file,
+   *    which may be overridden by
+   * 2. the `context` properties of the requested variants (if any) in the
+   *    component's definition file, which may be overridden by
+   * 3. the variables passed to the `render` tag itself.
+   *
+   * @param \Twig_Compiler $compiler
    */
   protected function addTemplateArguments(Twig_Compiler $compiler) {
-    $context = $this->getComponentContext($this->component);
-    $compiler
-      ->raw('array_merge($context,')
-      ->repr($context)
-      ->raw(',')
-      ->subcompile($this->getNode('variables'))
-      ->raw(')')
-    ;
-    return $compiler;
+    $defaults = $this->getComponentDefaults($this->component);
+    $compiler->raw('array_merge($context,')->repr($defaults);
+    if ($this->hasNode('variables')) {
+      $compiler->raw(',')->subcompile($this->getNode('variables'));
+    }
+    $compiler->raw(')');
   }
 
   /**
-   * Returns the component context from the component config file.
+   * Returns the component default variables from it's defintion file.
    *
    * @todo Merge specific keys to prevent config duplication.
+   *
+   * @param string $component
+   *   The component name.
+   *
+   * @return array
+   *   The default variables.
    */
-  public function getComponentContext($component) {
-    $context = [];
-    $sanitizedComponent = $this->sanitizeComponentName($component);
-    $config = Yaml::parse(file_get_contents($this->getComponentConfig($sanitizedComponent)));
+  public function getComponentDefaults($component) {
+    $defaults = [];
+    $componentName = $this->extractComponentName($component);
+    $componentDefinition = Yaml::parse(file_get_contents($this->getComponentDefinitionFile($componentName)));
     $component = $this->getComponentParts($component);
-    if ($component['variant'] && isset($config['variants'])) {
-      $context = $context + $this->getVariantContext($component['variant'], $config);
+    if ($component['variant'] && isset($componentDefinition['variants'])) {
+      $defaults += $this->getVariantDefaults($component['variant'], $componentDefinition['variants']);
     }
     else {
-      $context = $context + (array) $config['context'];
+      $defaults += (array) $componentDefinition['context'];
     }
-    return $context;
+    return $defaults;
   }
 
   /**
-   * Returns the component config path.
+   * Returns the relative file path of the Fractal YAML configuration file for a given component name.
+   *
+   * The filename ending must be `.config.yml`.
+   *
+   * @param string $component
+   *   The component name.
+   *
+   * @return string
+   *   The relative path for the component definition file.
    */
-  protected function getComponentConfig($component) {
+  protected function getComponentDefinitionFile($component) {
     $components = \Drupal::service('twig.loader.componentlibrary');
     $path = pathinfo($components->getCacheKey($component));
     return $path['dirname'] . '/' . $path['filename'] . '.config.yml';
   }
 
   /**
-   * Splits the component and return its parts.
+   * Splits the component and return the parts.
+   *
+   * @param string $component
+   *   The component name.
+   *
+   * @return array
+   *   The base and variant parts of the component.
    */
   public function getComponentParts($component) {
-    list($component, $variant) = explode('--', basename($component, '.twig'));
-    $parts = [
-      'base' => $component,
-      'variant' => $variant,
+    $parts = explode('--', basename($component, '.twig'));
+    return [
+      'base' => $parts[0] ?? NULL,
+      'variant' => $parts[1] ?? NULL,
     ];
     return $parts;
   }
 
   /**
-   * Returns the variant context from the config file.
+   * Returns the variant default variables from the parsed component definition.
+   *
+   * @param string $variantName
+   *   The variant name.
+   * @param array $variants
+   *   The parsed variants of the base component.
+   *
+   * @return array
+   *   The variant default variables.
    */
-  public function getVariantContext($variant, $config) {
-    $context = [];
-    foreach ($config['variants'] as $index => $variantInfo) {
+  public function getVariantDefaults($variantName, $variants) {
+    $defaults = [];
+    foreach ($variants as $index => $variant) {
       // Slugify the variant name to identify it from the component name.
-      // E.g. 'foo--baz-bar' will look for a the variant 'Baz Bar' in foo.config.yml
-      if (strtolower(Html::cleanCssIdentifier($variantInfo['name'])) === $variant) {
-        $context = $context + (array) $config['variants'][$index]['context'];
+      // E.g. `foo--baz-bar` will look for `Baz Bar` or `baz-bar` in
+      // the `variants` key in `foo.config.yml`.
+      if (strtolower(Html::cleanCssIdentifier($variant['name'])) === $variantName) {
+        $defaults += (array) $variants[$index]['context'];
         break;
       }
     }
-    return $context;
+    return $defaults;
   }
 
   /**
-   * Returns the sanitized component name
+   * Returns the extracted base component name without its variant.
+   *
+   * @param string $component
+   *   The component name.
+   *
+   * @return string
+   *   The base component name without its variant.
    */
-  public function sanitizeComponentName($component) {
-    return preg_replace('@--[a-z0-9-]+@', '', $component);
+  public function extractComponentName($component) {
+    return preg_replace('@--[a-z0-9-]+@i', '', $component);
   }
 
 }
--- src/TwigFractal.php
+++ src/TwigFractal.php
@@ -1,20 +1,43 @@
 <?php
+/**
+ * @file
+ * Contains Drupal\twig_fractal\TwigFractal.
+ */
 
 namespace Drupal\twig_fractal;
 
 use Drupal\twig_fractal\TokenParser\Render;
 use Twig_Extension;
 
+/**
+ * This class registers a custom `render` token parser to make Fractal components reusable in Drupal 8.
+ *
+ * @implements Twig_ExtensionInterface
+ *
+ * @see \Drupal\twig_fractal\TokenParser\Render
+ */
 class TwigFractal extends Twig_Extension {
 
-  public function getName() {
-    return 'twig_fractal';
-  }
-
+  /**
+   * Returns the `Render` token parser instance and add it to the existing list.
+   *
+   * @return array
+   *   The token parsers.
+   */
   public function getTokenParsers() {
     return [
       new Render(),
     ];
+  }
+
+  /**
+   * Returns the extension name.
+   *
+   * @return string
+   *   The extension name.
+   */
+  public function getName() {
+    return 'twig_fractal';
   }
 
 }
