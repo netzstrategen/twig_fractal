fbe9ea31997f2bb9c168e85d46131a52bff845ef
sun <daniel@netzstrategen.com>
2018-02-02 21:14:02 +0100

    Fixed docs, coding style.

--- src/TwigLoader.php
+++ src/TwigLoader.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\twig_fractal\Component.
+ * Contains \Drupal\twig_fractal\TwigLoader.
  */
 
 namespace Drupal\twig_fractal;
@@ -13,26 +13,27 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Extension\ThemeHandlerInterface;
 
 /**
- * Prepares paths to make fractal components overridable by a sub theme.
+ * Allows a subtheme to override Fractal components of its base theme.
  *
- * To make components overridable by a sub theme, the existing namespace paths
- * are getting prepended with adjusted sub theme paths.
+ * The filesystem path of a component that is overridden by the subtheme
+ * is prepended to the lookup paths of the component, so that it is found
+ * first.
  *
  * @see http://symfony.com/doc/current/templating/namespaced_paths.html#multiple-paths-per-namespace
  */
 class TwigLoader extends ComponentLibraryLoader {
 
   /**
-   * Prepends sub theme component paths to the existing base theme path namespaces.
-   *
    * {@inheritdoc}
    */
   public function __construct($paths = [], ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler) {
     parent::__construct($paths, $module_handler, $theme_handler);
+
     $default_theme = $theme_handler->getTheme($theme_handler->getDefault());
     if (!$default_theme->base_theme || !$base_theme = $theme_handler->getTheme($default_theme->base_theme)) {
       return;
     }
+
     foreach ($base_theme->info['component-libraries'] as $namespace => $component) {
       $paths = isset($component['paths']) ? $component['paths'] : [];
       foreach ($paths as $path) {
@@ -47,10 +48,12 @@ class TwigLoader extends ComponentLibraryLoader {
    */
   public function prependPath($path, $namespace = self::MAIN_NAMESPACE) {
     $this->cache = $this->errorCache = [];
+
     $path = rtrim($path, '/\\');
     if (!isset($this->paths[$namespace])) {
       $this->paths[$namespace][] = $path;
-    } else {
+    }
+    else {
       array_unshift($this->paths[$namespace], $path);
     }
   }
